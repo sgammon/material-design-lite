989951598b79995b29f8caef1788802327019b2b
Sam Gammon <sam@bloombox.io>
2019-05-18 16:33:42 -0700

    Fixes, round 2, for button.js

--- src/button/button.js
+++ src/button/button.js
@@ -14,109 +14,108 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function() {
-  'use strict';
-  var EventType = goog.require('goog.events.EventType');
-  var TagName = goog.require('goog.dom.TagName');
+goog.require('goog.events.EventType');
+goog.require('goog.dom.TagName');
 
+/**
+ * Store constants in one place so they can be updated easily.
+ *
+ * @enum {string | number}
+ * @private
+ */
+MaterialButtonConstant_ = {
+  // None for now.
+};
+
+/**
+ * Store strings for class names defined by this component that are used in
+ * JavaScript. This allows us to simply change it in one place should we
+ * decide to modify at a later date.
+ *
+ * @enum {string}
+ * @private
+ */
+MaterialButtonClasses_ = {
+  RIPPLE_EFFECT: goog.getCssName('mdl-js-ripple-effect'),
+  RIPPLE_CONTAINER: goog.getCssName('mdl-button__ripple-container'),
+  RIPPLE: goog.getCssName('mdl-ripple'),
+  JS_BUTTON: goog.getCssName('mdl-js-button')
+};
+
+/**
+ * Class constructor for Button MDL component.
+ * Implements MDL component design pattern defined at:
+ * https://github.com/jasonmayes/mdl-component-design-pattern
+ *
+ * @constructor
+ * @param {!HTMLElement} element The element that will be upgraded.
+ */
+var MaterialButton = function MaterialButton(element) {
   /**
-   * Store constants in one place so they can be updated easily.
+   * Element that will be upgraded.
    *
-   * @enum {string | number}
+   * @const
    * @private
+   * @type {!HTMLElement}
    */
-  MaterialButtonConstant_ = {
-    // None for now.
-  };
+  this.element_ = element;
 
-  /**
-   * Store strings for class names defined by this component that are used in
-   * JavaScript. This allows us to simply change it in one place should we
-   * decide to modify at a later date.
-   *
-   * @enum {string}
-   * @private
-   */
-  MaterialButtonClasses_ = {
-    RIPPLE_EFFECT: goog.getCssName('mdl-js-ripple-effect'),
-    RIPPLE_CONTAINER: goog.getCssName('mdl-button__ripple-container'),
-    RIPPLE: goog.getCssName('mdl-ripple'),
-    JS_BUTTON: goog.getCssName('mdl-js-button')
-  };
-
-  /**
-   * Class constructor for Button MDL component.
-   * Implements MDL component design pattern defined at:
-   * https://github.com/jasonmayes/mdl-component-design-pattern
-   *
-   * @constructor
-   * @param {!HTMLElement} element The element that will be upgraded.
-   */
-  var MaterialButton = function MaterialButton(element) {
-    /**
-     * Element that will be upgraded.
-     *
-     * @const
-     * @type {!HTMLElement}
-     */
-    this.element_ = element;
-
-    // Initialize instance.
-    if (this.element_) {
-      if (this.element_.classList.contains(MaterialButtonClasses_.RIPPLE_EFFECT)) {
-        var rippleContainer = document.createElement(goog.dom.TagName.SPAN);
-        rippleContainer.classList.add(MaterialButtonClasses_.RIPPLE_CONTAINER);
-        this.rippleElement_ = document.createElement(goog.dom.TagName.SPAN);
-        this.rippleElement_.classList.add(MaterialButtonClasses_.RIPPLE);
-        rippleContainer.appendChild(this.rippleElement_);
-        this.boundRippleBlurHandler = this.blurHandler_.bind(this);
-        this.rippleElement_.addEventListener(goog.events.EventType.MOUSEUP, this.boundRippleBlurHandler);
-        this.element_.appendChild(rippleContainer);
-      }
-      this.boundButtonBlurHandler = this.blurHandler_.bind(this);
-      this.element_.addEventListener(goog.events.EventType.MOUSEUP, this.boundButtonBlurHandler);
-      this.element_.addEventListener(goog.events.EventType.MOUSELEAVE, this.boundButtonBlurHandler);
+  // Initialize instance.
+  if (this.element_) {
+    if (this.element_.classList.contains(MaterialButtonClasses_.RIPPLE_EFFECT)) {
+      var rippleContainer = document.createElement(goog.dom.TagName.SPAN);
+      rippleContainer.classList.add(MaterialButtonClasses_.RIPPLE_CONTAINER);
+      this.rippleElement_ = document.createElement(goog.dom.TagName.SPAN);
+      this.rippleElement_.classList.add(MaterialButtonClasses_.RIPPLE);
+      rippleContainer.appendChild(this.rippleElement_);
+      this.boundRippleBlurHandler = this.blurHandler_.bind(this);
+      this.rippleElement_.addEventListener(goog.events.EventType.MOUSEUP, this.boundRippleBlurHandler);
+      this.element_.appendChild(rippleContainer);
     }
-  };
+    this.boundButtonBlurHandler = this.blurHandler_.bind(this);
+    this.element_.addEventListener(goog.events.EventType.MOUSEUP, this.boundButtonBlurHandler);
+    this.element_.addEventListener(goog.events.EventType.MOUSELEAVE, this.boundButtonBlurHandler);
+  }
+};
 
-  /**
-   * Handle blur of element.
-   *
-   * @param {Event} event The event that fired.
-   * @private
-   */
-  MaterialButton.prototype.blurHandler_ = function(event) {
-    if (event) {
-      this.element_.blur();
-    }
-  };
+/**
+ * Handle blur of element.
+ *
+ * @param {Event} event The event that fired.
+ * @private
+ */
+MaterialButton.prototype.blurHandler_ = function(event) {
+  if (event) {
+    this.element_.blur();
+  }
+};
 
-  // Public methods.
+// Public methods.
 
-  /**
-   * Disable button.
-   *
-   * @public
-   */
-  MaterialButton.prototype.disable = function() {
-    this.element_.disabled = true;
-  };
+/**
+ * Disable button.
+ *
+ * @public
+ */
+MaterialButton.prototype.disable = function() {
+  this.element_.disabled = true;
+};
 
-  /**
-   * Enable button.
-   *
-   * @public
-   */
-  MaterialButton.prototype.enable = function() {
-    this.element_.disabled = false;
-  };
+/**
+ * Enable button.
+ *
+ * @public
+ */
+MaterialButton.prototype.enable = function() {
+  this.element_.disabled = false;
+};
 
-  // The component registers itself. It can assume componentHandler is available
-  // in the global scope.
-  componentHandler.register({
-    constructor: MaterialButton,
-    classAsString: 'MaterialButton',
-    cssClass: MaterialButtonClasses_.JS_BUTTON,
-    widget: true
-  });
-})();
+// The component registers itself. It can assume componentHandler is available
+// in the global scope.
+componentHandler.register({
+  constructor: MaterialButton,
+  classAsString: 'MaterialButton',
+  cssClass: MaterialButtonClasses_.JS_BUTTON,
+  widget: true
+});
+
