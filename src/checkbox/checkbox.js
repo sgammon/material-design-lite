43ccdb9c006585f365e635b80dc2adae37b38cd8
Sam Gammon <sam@bloombox.io>
2019-05-23 10:50:11 -0700

    Support for MDL checkboxes
    - Related to Bloombox/DENDRITE#24
    - Related to Bloombox/DENDRITE#3

--- src/checkbox/checkbox.js
+++ src/checkbox/checkbox.js
@@ -14,255 +14,256 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function() {
-  'use strict';
+
+/* global goog */
+
+goog.require('goog.dom.TagName');
+goog.require('goog.events.EventType');
+
+goog.provide('material.MaterialCheckbox');
+
+
+/**
+ * Timeout value for some checkbox operations/animations.
+ *
+ * @const {number}
+ * @private
+ */
+const TINY_TIMEOUT = 0.001;
+
+/**
+ * Store strings for class names defined by this component that are used in
+ * JavaScript. This allows us to simply change it in one place should we
+ * decide to modify at a later date.
+ *
+ * @enum {string}
+ * @private
+ */
+const MaterialCheckboxCssClasses_ = {
+  INPUT: goog.getCssName('mdl-checkbox__input'),
+  BOX_OUTLINE: goog.getCssName('mdl-checkbox__box-outline'),
+  FOCUS_HELPER: goog.getCssName('mdl-checkbox__focus-helper'),
+  TICK_OUTLINE: goog.getCssName('mdl-checkbox__tick-outline'),
+  RIPPLE_EFFECT: goog.getCssName('mdl-js-ripple-effect'),
+  RIPPLE_IGNORE_EVENTS: goog.getCssName('mdl-js-ripple-effect--ignore-events'),
+  RIPPLE_CONTAINER: goog.getCssName('mdl-checkbox__ripple-container'),
+  RIPPLE_CENTER: goog.getCssName('mdl-ripple--center'),
+  RIPPLE: goog.getCssName('mdl-ripple'),
+  IS_FOCUSED: goog.getCssName('is-focused'),
+  IS_DISABLED: goog.getCssName('is-disabled'),
+  IS_CHECKED: goog.getCssName('is-checked'),
+  IS_UPGRADED: goog.getCssName('is-upgraded')
+};
+
+/**
+ * Class constructor for Checkbox MDL component.
+ * Implements MDL component design pattern defined at:
+ * https://github.com/jasonmayes/mdl-component-design-pattern
+ *
+ * @constructor
+ * @param {!HTMLElement} element The element that will be upgraded.
+ */
+material.MaterialCheckbox = function MaterialCheckbox(element) {
+  const spanTag = goog.dom.TagName.SPAN.toString();
 
   /**
-   * Class constructor for Checkbox MDL component.
-   * Implements MDL component design pattern defined at:
-   * https://github.com/jasonmayes/mdl-component-design-pattern
+   * Root element to attach the checkbox to.
    *
-   * @constructor
-   * @param {HTMLElement} element The element that will be upgraded.
-   */
-  var MaterialCheckbox = function MaterialCheckbox(element) {
-    this.element_ = element;
-
-    // Initialize instance.
-    this.init();
-  };
-  window['MaterialCheckbox'] = MaterialCheckbox;
-
-  /**
-   * Store constants in one place so they can be updated easily.
-   *
-   * @enum {string | number}
+   * @const
+   * @type {!HTMLElement}
    * @private
    */
-  MaterialCheckbox.prototype.Constant_ = {
-    TINY_TIMEOUT: 0.001
-  };
+  this.element_ = element;
 
   /**
-   * Store strings for class names defined by this component that are used in
-   * JavaScript. This allows us to simply change it in one place should we
-   * decide to modify at a later date.
+   * Input element to use under the root element.
    *
-   * @enum {string}
+   * @const
+   * @type {!HTMLInputElement}
    * @private
    */
-  MaterialCheckbox.prototype.CssClasses_ = {
-    INPUT: 'mdl-checkbox__input',
-    BOX_OUTLINE: 'mdl-checkbox__box-outline',
-    FOCUS_HELPER: 'mdl-checkbox__focus-helper',
-    TICK_OUTLINE: 'mdl-checkbox__tick-outline',
-    RIPPLE_EFFECT: 'mdl-js-ripple-effect',
-    RIPPLE_IGNORE_EVENTS: 'mdl-js-ripple-effect--ignore-events',
-    RIPPLE_CONTAINER: 'mdl-checkbox__ripple-container',
-    RIPPLE_CENTER: 'mdl-ripple--center',
-    RIPPLE: 'mdl-ripple',
-    IS_FOCUSED: 'is-focused',
-    IS_DISABLED: 'is-disabled',
-    IS_CHECKED: 'is-checked',
-    IS_UPGRADED: 'is-upgraded'
-  };
+  this.inputElement_ = /** @type {!HTMLInputElement} */ (
+    this.element_.querySelector('.' + MaterialCheckboxCssClasses_.INPUT));
 
-  /**
-   * Handle change of state.
-   *
-   * @param {Event} event The event that fired.
-   * @private
-   */
-  MaterialCheckbox.prototype.onChange_ = function(event) {
-    this.updateClasses_();
-  };
+  const boxOutline = document.createElement(spanTag);
+  boxOutline.classList.add(MaterialCheckboxCssClasses_.BOX_OUTLINE);
 
-  /**
-   * Handle focus of element.
-   *
-   * @param {Event} event The event that fired.
-   * @private
-   */
-  MaterialCheckbox.prototype.onFocus_ = function(event) {
-    this.element_.classList.add(this.CssClasses_.IS_FOCUSED);
-  };
+  const tickContainer = document.createElement(spanTag);
+  tickContainer.classList.add(MaterialCheckboxCssClasses_.FOCUS_HELPER);
 
-  /**
-   * Handle lost focus of element.
-   *
-   * @param {Event} event The event that fired.
-   * @private
-   */
-  MaterialCheckbox.prototype.onBlur_ = function(event) {
-    this.element_.classList.remove(this.CssClasses_.IS_FOCUSED);
-  };
+  const tickOutline = document.createElement(spanTag);
+  tickOutline.classList.add(MaterialCheckboxCssClasses_.TICK_OUTLINE);
 
-  /**
-   * Handle mouseup.
-   *
-   * @param {Event} event The event that fired.
-   * @private
-   */
-  MaterialCheckbox.prototype.onMouseUp_ = function(event) {
-    this.blur_();
-  };
+  boxOutline.appendChild(tickOutline);
 
-  /**
-   * Handle class updates.
-   *
-   * @private
-   */
-  MaterialCheckbox.prototype.updateClasses_ = function() {
-    this.checkDisabled();
-    this.checkToggleState();
-  };
+  this.element_.appendChild(tickContainer);
+  this.element_.appendChild(boxOutline);
 
-  /**
-   * Add blur.
-   *
-   * @private
-   */
-  MaterialCheckbox.prototype.blur_ = function() {
-    // TODO: figure out why there's a focus event being fired after our blur,
-    // so that we can avoid this hack.
-    window.setTimeout(function() {
-      this.inputElement_.blur();
-    }.bind(this), /** @type {number} */ (this.Constant_.TINY_TIMEOUT));
-  };
+  if (this.element_.classList.contains(MaterialCheckboxCssClasses_.RIPPLE_EFFECT)) {
+    this.element_.classList.add(MaterialCheckboxCssClasses_.RIPPLE_IGNORE_EVENTS);
+    this.rippleContainerElement_ = document.createElement(spanTag);
+    this.rippleContainerElement_.classList.add(MaterialCheckboxCssClasses_.RIPPLE_CONTAINER);
+    this.rippleContainerElement_.classList.add(MaterialCheckboxCssClasses_.RIPPLE_EFFECT);
+    this.rippleContainerElement_.classList.add(MaterialCheckboxCssClasses_.RIPPLE_CENTER);
+    this.boundRippleMouseUp = this.onMouseUp_.bind(this);
+    this.rippleContainerElement_.addEventListener(goog.events.EventType.MOUSEUP, this.boundRippleMouseUp);
 
-  // Public methods.
+    const ripple = document.createElement(spanTag);
+    ripple.classList.add(MaterialCheckboxCssClasses_.RIPPLE);
 
-  /**
-   * Check the inputs toggle state and update display.
-   *
-   * @public
-   */
-  MaterialCheckbox.prototype.checkToggleState = function() {
-    if (this.inputElement_.checked) {
-      this.element_.classList.add(this.CssClasses_.IS_CHECKED);
-    } else {
-      this.element_.classList.remove(this.CssClasses_.IS_CHECKED);
-    }
-  };
-  MaterialCheckbox.prototype['checkToggleState'] =
-      MaterialCheckbox.prototype.checkToggleState;
+    this.rippleContainerElement_.appendChild(ripple);
+    this.element_.appendChild(this.rippleContainerElement_);
+  }
+  this.boundInputOnChange = this.onChange_.bind(this);
+  this.boundInputOnFocus = this.onFocus_.bind(this);
+  this.boundInputOnBlur = this.onBlur_.bind(this);
+  this.boundElementMouseUp = this.onMouseUp_.bind(this);
+  this.inputElement_.addEventListener(goog.events.EventType.CHANGE, this.boundInputOnChange);
+  this.inputElement_.addEventListener(goog.events.EventType.FOCUS, this.boundInputOnFocus);
+  this.inputElement_.addEventListener(goog.events.EventType.BLUR, this.boundInputOnBlur);
+  this.element_.addEventListener(goog.events.EventType.MOUSEUP, this.boundElementMouseUp);
 
-  /**
-   * Check the inputs disabled state and update display.
-   *
-   * @public
-   */
-  MaterialCheckbox.prototype.checkDisabled = function() {
-    if (this.inputElement_.disabled) {
-      this.element_.classList.add(this.CssClasses_.IS_DISABLED);
-    } else {
-      this.element_.classList.remove(this.CssClasses_.IS_DISABLED);
-    }
-  };
-  MaterialCheckbox.prototype['checkDisabled'] =
-      MaterialCheckbox.prototype.checkDisabled;
+  this.updateClasses_();
+  this.element_.classList.add(MaterialCheckboxCssClasses_.IS_UPGRADED);
+};
 
-  /**
-   * Disable checkbox.
-   *
-   * @public
-   */
-  MaterialCheckbox.prototype.disable = function() {
-    this.inputElement_.disabled = true;
-    this.updateClasses_();
-  };
-  MaterialCheckbox.prototype['disable'] = MaterialCheckbox.prototype.disable;
+/**
+ * Handle change of state.
+ *
+ * @param {Event} event The event that fired.
+ * @private
+ */
+material.MaterialCheckbox.prototype.onChange_ = function(event) {
+  this.updateClasses_();
+};
 
-  /**
-   * Enable checkbox.
-   *
-   * @public
-   */
-  MaterialCheckbox.prototype.enable = function() {
-    this.inputElement_.disabled = false;
-    this.updateClasses_();
-  };
-  MaterialCheckbox.prototype['enable'] = MaterialCheckbox.prototype.enable;
+/**
+ * Handle focus of element.
+ *
+ * @param {Event} event The event that fired.
+ * @private
+ */
+material.MaterialCheckbox.prototype.onFocus_ = function(event) {
+  this.element_.classList.add(this.CssClasses_.IS_FOCUSED);
+};
 
-  /**
-   * Check checkbox.
-   *
-   * @public
-   */
-  MaterialCheckbox.prototype.check = function() {
-    this.inputElement_.checked = true;
-    this.updateClasses_();
-  };
-  MaterialCheckbox.prototype['check'] = MaterialCheckbox.prototype.check;
+/**
+ * Handle lost focus of element.
+ *
+ * @param {Event} event The event that fired.
+ * @private
+ */
+material.MaterialCheckbox.prototype.onBlur_ = function(event) {
+  this.element_.classList.remove(this.CssClasses_.IS_FOCUSED);
+};
 
-  /**
-   * Uncheck checkbox.
-   *
-   * @public
-   */
-  MaterialCheckbox.prototype.uncheck = function() {
-    this.inputElement_.checked = false;
-    this.updateClasses_();
-  };
-  MaterialCheckbox.prototype['uncheck'] = MaterialCheckbox.prototype.uncheck;
+/**
+ * Handle mouseup.
+ *
+ * @param {Event} event The event that fired.
+ * @private
+ */
+material.MaterialCheckbox.prototype.onMouseUp_ = function(event) {
+  this.blur_();
+};
 
-  /**
-   * Initialize element.
-   */
-  MaterialCheckbox.prototype.init = function() {
-    if (this.element_) {
-      this.inputElement_ = this.element_.querySelector('.' +
-          this.CssClasses_.INPUT);
+/**
+ * Handle class updates.
+ *
+ * @private
+ */
+material.MaterialCheckbox.prototype.updateClasses_ = function() {
+  this.checkDisabled();
+  this.checkToggleState();
+};
 
-      var boxOutline = document.createElement('span');
-      boxOutline.classList.add(this.CssClasses_.BOX_OUTLINE);
+/**
+ * Add blur.
+ *
+ * @private
+ */
+material.MaterialCheckbox.prototype.blur_ = function() {
+  // TODO: figure out why there's a focus event being fired after our blur,
+  // so that we can avoid this hack.
+  window.setTimeout(function() {
+    this.inputElement_.blur();
+  }.bind(this), /** @type {number} */ (TINY_TIMEOUT));
+};
 
-      var tickContainer = document.createElement('span');
-      tickContainer.classList.add(this.CssClasses_.FOCUS_HELPER);
+// Public methods.
 
-      var tickOutline = document.createElement('span');
-      tickOutline.classList.add(this.CssClasses_.TICK_OUTLINE);
+/**
+ * Indicate whether the checkbox is currently checked.
+ *
+ * @public
+ * @return {boolean}
+ */
+material.MaterialCheckbox.prototype.isChecked = function() {
+  return !!this.inputElement_.checked;
+};
 
-      boxOutline.appendChild(tickOutline);
+/**
+ * Check the inputs toggle state and update display.
+ *
+ * @public
+ */
+material.MaterialCheckbox.prototype.checkToggleState = function() {
+  if (this.inputElement_.checked) {
+    this.element_.classList.add(this.CssClasses_.IS_CHECKED);
+  } else {
+    this.element_.classList.remove(this.CssClasses_.IS_CHECKED);
+  }
+};
 
-      this.element_.appendChild(tickContainer);
-      this.element_.appendChild(boxOutline);
+/**
+ * Check the inputs disabled state and update display.
+ *
+ * @public
+ */
+material.MaterialCheckbox.prototype.checkDisabled = function() {
+  if (this.inputElement_.disabled) {
+    this.element_.classList.add(this.CssClasses_.IS_DISABLED);
+  } else {
+    this.element_.classList.remove(this.CssClasses_.IS_DISABLED);
+  }
+};
 
-      if (this.element_.classList.contains(this.CssClasses_.RIPPLE_EFFECT)) {
-        this.element_.classList.add(this.CssClasses_.RIPPLE_IGNORE_EVENTS);
-        this.rippleContainerElement_ = document.createElement('span');
-        this.rippleContainerElement_.classList.add(this.CssClasses_.RIPPLE_CONTAINER);
-        this.rippleContainerElement_.classList.add(this.CssClasses_.RIPPLE_EFFECT);
-        this.rippleContainerElement_.classList.add(this.CssClasses_.RIPPLE_CENTER);
-        this.boundRippleMouseUp = this.onMouseUp_.bind(this);
-        this.rippleContainerElement_.addEventListener('mouseup', this.boundRippleMouseUp);
+/**
+ * Disable checkbox.
+ *
+ * @public
+ */
+material.MaterialCheckbox.prototype.disable = function() {
+  this.inputElement_.disabled = true;
+  this.updateClasses_();
+};
 
-        var ripple = document.createElement('span');
-        ripple.classList.add(this.CssClasses_.RIPPLE);
+/**
+ * Enable checkbox.
+ *
+ * @public
+ */
+material.MaterialCheckbox.prototype.enable = function() {
+  this.inputElement_.disabled = false;
+  this.updateClasses_();
+};
 
-        this.rippleContainerElement_.appendChild(ripple);
-        this.element_.appendChild(this.rippleContainerElement_);
-      }
-      this.boundInputOnChange = this.onChange_.bind(this);
-      this.boundInputOnFocus = this.onFocus_.bind(this);
-      this.boundInputOnBlur = this.onBlur_.bind(this);
-      this.boundElementMouseUp = this.onMouseUp_.bind(this);
-      this.inputElement_.addEventListener('change', this.boundInputOnChange);
-      this.inputElement_.addEventListener('focus', this.boundInputOnFocus);
-      this.inputElement_.addEventListener('blur', this.boundInputOnBlur);
-      this.element_.addEventListener('mouseup', this.boundElementMouseUp);
+/**
+ * Check checkbox.
+ *
+ * @public
+ */
+material.MaterialCheckbox.prototype.check = function() {
+  this.inputElement_.checked = true;
+  this.updateClasses_();
+};
 
-      this.updateClasses_();
-      this.element_.classList.add(this.CssClasses_.IS_UPGRADED);
-    }
-  };
-
-  // The component registers itself. It can assume componentHandler is available
-  // in the global scope.
-  componentHandler.register({
-    constructor: MaterialCheckbox,
-    classAsString: 'MaterialCheckbox',
-    cssClass: 'mdl-js-checkbox',
-    widget: true
-  });
-})();
+/**
+ * Uncheck checkbox.
+ *
+ * @public
+ */
+material.MaterialCheckbox.prototype.uncheck = function() {
+  this.inputElement_.checked = false;
+  this.updateClasses_();
+};
